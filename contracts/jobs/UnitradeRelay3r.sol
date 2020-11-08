d52a052b7bab4038ba12ffa2ba5e71b3eed0d431
akshaynexus <akshaycm@hotmail.com>
2020-11-09 00:25:15 +0300

    Add intial draft of unitrade job

---
+++ contracts/jobs/UnitradeRelay3r.sol
@@ -0,0 +1,166 @@
+pragma solidity ^0.6.12;
+import "../libraries/UniswapV2Library.sol";
+import '../interfaces/Keep3r/IKeep3rV1Mini.sol';
+
+enum OrderState {Placed, Cancelled, Executed}
+
+interface UnitradeInterface {
+    function cancelOrder(uint256 orderId) external returns (bool);
+
+    function executeOrder(uint256 orderId)
+        external
+        returns (uint256[] memory amounts);
+
+    function feeDiv() external view returns (uint16);
+
+    function feeMul() external view returns (uint16);
+
+    function getActiveOrderId(uint256 index) external view returns (uint256);
+
+    function getActiveOrdersLength() external view returns (uint256);
+
+    function getOrder(uint256 orderId)
+        external
+        view
+        returns (
+            uint8 orderType,
+            address maker,
+            address tokenIn,
+            address tokenOut,
+            uint256 amountInOffered,
+            uint256 amountOutExpected,
+            uint256 executorFee,
+            uint256 totalEthDeposited,
+            OrderState orderState,
+            bool deflationary
+        );
+
+    function getOrderIdForAddress(address _address, uint256 index)
+        external
+        view
+        returns (uint256);
+
+    function getOrdersForAddressLength(address _address)
+        external
+        view
+        returns (uint256);
+
+    function incinerator() external view returns (address);
+
+    function owner() external view returns (address);
+
+    function placeOrder(
+        uint8 orderType,
+        address tokenIn,
+        address tokenOut,
+        uint256 amountInOffered,
+        uint256 amountOutExpected,
+        uint256 executorFee
+    ) external returns (uint256);
+
+    function renounceOwnership() external;
+
+    function splitDiv() external view returns (uint16);
+
+    function splitMul() external view returns (uint16);
+
+    function staker() external view returns (address);
+
+    function transferOwnership(address newOwner) external;
+
+    function uniswapV2Factory() external view returns (address);
+
+    function uniswapV2Router() external view returns (address);
+
+    function updateFee(uint16 _feeMul, uint16 _feeDiv) external;
+
+    function updateOrder(
+        uint256 orderId,
+        uint256 amountInOffered,
+        uint256 amountOutExpected,
+        uint256 executorFee
+    ) external returns (bool);
+
+    function updateSplit(uint16 _splitMul, uint16 _splitDiv) external;
+
+    function updateStaker(address newStaker) external;
+}
+
+contract UnitradeRelay3r {
+    UnitradeInterface iUniTrade = UnitradeInterface(
+        0xC1bF1B4929DA9303773eCEa5E251fDEc22cC6828
+    );
+    //change this to relay3r on deploy
+    IKeep3rV1Mini constant public KP3R = IKeep3rV1Mini(0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44);
+
+    modifier upkeep() {
+        require(KP3R.isKeeper(msg.sender), "::isKeeper: keeper is not registered");
+        _;
+        KP3R.worked(msg.sender);
+    }
+
+    function getIfExecuteable(uint256 i) public view returns (bool) {
+        (
+            uint8 orderType,
+            address maker,
+            address tokenIn,
+            address tokenOut,
+            uint256 amountInOffered,
+            uint256 amountOutExpected,
+            uint256 executorFee,
+            uint256 totalEthDeposited,
+            OrderState orderState,
+            bool deflationary
+        ) = iUniTrade.getOrder(i);
+        address[] memory path = new address[](2);
+        path[0] = tokenIn;
+        path[1] = tokenOut;
+        if(executorFee <= 0) return false;//Dont execute unprofitable orders
+        uint256[] memory amounts = UniswapV2Library.getAmountsOut(
+            iUniTrade.uniswapV2Factory(),
+            amountInOffered,
+            path
+        );
+        if (
+            amounts[1] >= amountOutExpected && orderState == OrderState.Placed
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    function getExecutableCount() public view returns (uint256 count) {
+        count = 0;
+        for (uint256 i = 0; i < iUniTrade.getActiveOrdersLength(); i++) {
+            if (getIfExecuteable(i)) {
+                count++;
+            }
+        }
+    }
+
+    function workable() public view returns (bool) {
+        return getExecutableCount() > 0;
+    }
+
+    function work() public upkeep{
+        require(workable(),"No executable trades");
+        for (uint256 i = 0; i < iUniTrade.getActiveOrdersLength(); i++) {
+            if (getIfExecuteable(i)) {
+                iUniTrade.executeOrder(i);
+            }
+        }
+    }
+
+    // function getExecutablePairs() public view returns (uint256[] memory pairs) {
+    //     uint256[] storage executeablepairs;
+    //     executeablepairs.push();
+    //     for (uint256 i = 0; i < iUniTrade.getActiveOrdersLength(); i++) {
+    //         if (getIfExecuteable(i)) {
+    //             executeablepairs.push(i);
+    //         }
+    //     }
+    //     for (uint256 j = 0; j < executeablepairs.length; j++) {
+    //         pairs[j] = executeablepairs[j];
+    //     }
+    // }
+}
