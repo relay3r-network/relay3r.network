1e8fa86e6c25c41072264287e3c36a6fa68fa7e4
akshaynexus <akshaycm@hotmail.com>
2020-11-12 05:18:12 +0300

    Add tokenmigrator contract Implement CoreFlashArbRelay3r job

---
+++ contracts/jobs/CoreFlashArbRelay3r.sol
@@ -0,0 +1,35 @@
+import '../interfaces/Keep3r/IKeep3rV1Mini.sol';
+import '../interfaces/ICoreFlashArb.sol';
+
+contract CoreFlashArbRelay3r {
+
+    modifier upkeep() {
+        require(RL3R.isKeeper(msg.sender), "::isKeeper: keeper is not registered");
+        _;
+        RL3R.worked(msg.sender);
+    }
+
+    IKeep3rV1Mini public RL3R;
+    ICoreFlashArb public CoreArb;
+    //Init interfaces with addresses
+    constructor (address token,address corearb) public {
+        RL3R = IKeep3rV1Mini(token);
+        CoreArb = ICoreFlashArb(corearb);
+    }
+
+    function workable() public view returns (bool){
+        for(uint i=0;i<CoreArb.numberOfStrategies();i++){
+            if(CoreArb.strategyProfitInReturnToken(i) > 0)
+                return true;
+        }
+    }
+
+    function work() public upkeep {
+        require(workable(),"No profitable arb strat");
+        for(uint i=0;i<CoreArb.numberOfStrategies();i++){
+            if(CoreArb.strategyProfitInReturnToken(i) > 0)
+                CoreArb.executeStrategy(i);
+        }
+    }
+
+}
