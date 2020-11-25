f5cce6b744b3363a5761f4d04ce536c1444632fe
akshaynexus <akshaycm@hotmail.com>
2020-11-25 12:12:42 +0300

    Prettier on deploy contracts file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -19,7 +19,9 @@ const RlrMooniRewards = artifacts.require("RlrMooniRewards");
 //Jobs
 const UnitradeRelay3r = artifacts.require("UnitradeRelay3r");
 const UniswapV2SlidingOracle = artifacts.require("UniswapV2SlidingOracle");
-const CoreFlashArbRelay3rOptNew = artifacts.require("CoreFlashArbRelay3rOptNew");
+const CoreFlashArbRelay3rOptNew = artifacts.require(
+  "CoreFlashArbRelay3rOptNew"
+);
 
 //LP contracts
 // const GetRL3RLPs = artifacts.require("GetRelay3rLPTokens");
@@ -27,17 +29,16 @@ const CoreFlashArbRelay3rOptNew = artifacts.require("CoreFlashArbRelay3rOptNew")
 
 const Addrs = require("../constants/constants").Addrs;
 
-
 /* Various deploy stages */
 const InitialDeployWithMigrator = false;
 const TestMigrator = false;
 const DeployLiqMigrator = false;
-const DeployLegacyHelper =false;
+const DeployLegacyHelper = false;
 const DeployNewCoreJob = false;
 const DeployLiqMiner = true;
 const testLiqMinerPhase = false;
 module.exports = async function (deployer) {
-      // Deploy token with library
+  // Deploy token with library
   if (InitialDeployWithMigrator) {
     //Deploy v2 token
     await deployer.deploy(Keep3rV1Library);
@@ -70,7 +71,12 @@ module.exports = async function (deployer) {
     await RelayerTokenD.addJob(UnitradeJob.address);
 
     //Deploy CoreFlashArbRelay3r
-    await deployer.deploy(CoreFlashArbRelay3rOptNew, RelayerTokenD.address,Addrs.CoreFlashArb[1],Addrs.CoreToken[1]);
+    await deployer.deploy(
+      CoreFlashArbRelay3rOptNew,
+      RelayerTokenD.address,
+      Addrs.CoreFlashArb[1],
+      Addrs.CoreToken[1]
+    );
     const CoreFlashArbRelay3rOptNew = await CoreFlashArbRelay3rOptNew.deployed();
     //Add to jobs on keeper token
     await RelayerTokenD.addJob(CoreFlashArbRelay3rOptNew.address);
@@ -126,7 +132,10 @@ module.exports = async function (deployer) {
     await TokenMigratorD.SetSwapToken(RelayerTokenD.address);
     await TokenMigratorD.setBurn(true);
     //Now send supply of relayer tokens to migrator
-    await RelayerTokenD.transfer(TokenMigratorD.address,Web3.utils.toWei("109965", "ether"));
+    await RelayerTokenD.transfer(
+      TokenMigratorD.address,
+      Web3.utils.toWei("109965", "ether")
+    );
 
     // //Add 8 RLR on SushiswapV2Keep3r Job
     // await RelayerTokenD.addRLRCredit(
@@ -138,56 +147,62 @@ module.exports = async function (deployer) {
     //   YearnV1EarnKeep3rJob.address,
     //   Web3.utils.toWei("10", "ether")
     // );
-  }
-
-  else if (TestMigrator){
+  } else if (TestMigrator) {
     //Deploy TokenMigrator
     await deployer.deploy(TokenMigrator);
     const TokenMigratorD = await TokenMigrator.deployed();
 
     ///Now deploy test token 1
-    await deployer.deploy(BurnableToken,"TestToken1","TXS");
+    await deployer.deploy(BurnableToken, "TestToken1", "TXS");
     const Token1 = await BurnableToken.deployed();
-    await deployer.deploy(BurnableToken,"TestToken2","TXSS");
+    await deployer.deploy(BurnableToken, "TestToken2", "TXSS");
     const Token2 = await BurnableToken.deployed();
 
     await TokenMigratorD.SetOriginToken(Token1.address);
     await TokenMigratorD.SetSwapToken(Token2.address);
 
     //Now transfer 1k tokens of token2 to migrator
-    await Token2.transfer(TokenMigratorD.address,Web3.utils.toWei("1000", "ether"));
+    await Token2.transfer(
+      TokenMigratorD.address,
+      Web3.utils.toWei("1000", "ether")
+    );
     //Approve 1k tokens to migrator contract
-    await Token1.approve(TokenMigratorD.address,Web3.utils.toWei("1000", "ether"));
+    await Token1.approve(
+      TokenMigratorD.address,
+      Web3.utils.toWei("1000", "ether")
+    );
     //Swap 1k tokens to token2
     // await TokenMigratorD.unpauseSwap();
     await TokenMigratorD.swapTokens(Web3.utils.toWei("1000", "ether"));
 
-    const Token1SupplyAfterMigrate = await Token1.totalSupply()
-    const Token2SupplyAfterMigrate = await Token2.totalSupply()
+    const Token1SupplyAfterMigrate = await Token1.totalSupply();
+    const Token2SupplyAfterMigrate = await Token2.totalSupply();
 
     //Check if migration was successful
-    console.log(Token1SupplyAfterMigrate.toString() === "0")//Supply of token 1 to be 0 after migration
-    console.log(Token2SupplyAfterMigrate.toString() === "1000000000000000000000") //supply of token 2 to be 1k after migration
-
-  }
-  else if (DeployLiqMigrator){
+    console.log(Token1SupplyAfterMigrate.toString() === "0"); //Supply of token 1 to be 0 after migration
+    console.log(
+      Token2SupplyAfterMigrate.toString() === "1000000000000000000000"
+    ); //supply of token 2 to be 1k after migration
+  } else if (DeployLiqMigrator) {
     await deployer.deploy(LiqMigratorNew);
-  }
-
-  else if (DeployLegacyHelper){
+  } else if (DeployLegacyHelper) {
     const RelayerTokenD = await Relay3rV2.at(Addrs.RLRToken[1]);
     await deployer.deploy(Keep3rV1HelperLegacyV1);
     const keeperHelperD = await Keep3rV1HelperLegacyV1.deployed();
     await RelayerTokenD.setKeep3rHelper(keeperHelperD.address);
-
-  }
-
-  else if (DeployNewCoreJob){
+  } else if (DeployNewCoreJob) {
     const RelayerTokenD = await Relay3rV2.at(Addrs.RLRToken[1]);
-    const KeeperJobRegistryD = await Keep3rV1JobRegistry.at(Addrs.Keep3rV1JobRegistry[1]);
+    const KeeperJobRegistryD = await Keep3rV1JobRegistry.at(
+      Addrs.Keep3rV1JobRegistry[1]
+    );
 
     //Deploy CoreFlashArbRelay3rNew
-    await deployer.deploy(CoreFlashArbRelay3rOptNew, RelayerTokenD.address,Addrs.CoreFlashArb[1],Addrs.CoreToken[1]);
+    await deployer.deploy(
+      CoreFlashArbRelay3rOptNew,
+      RelayerTokenD.address,
+      Addrs.CoreFlashArb[1],
+      Addrs.CoreToken[1]
+    );
     const CoreFlashArbRelay3rOptNewJob = await CoreFlashArbRelay3rOptNew.deployed();
     //Remove old job
     await RelayerTokenD.removeJob("0x7905AAE5E92D9Ff324d0b2Ae5220e2Bb0078553a");
@@ -196,7 +211,7 @@ module.exports = async function (deployer) {
     //Add 1 RLR on CoreFlashArbRelay3rNew Job
     await RelayerTokenD.addRLRCredit(
       CoreFlashArbRelay3rOptNewJob.address,
-        Web3.utils.toWei("1", "ether")
+      Web3.utils.toWei("1", "ether")
     );
     //Add to registry
     await KeeperJobRegistryD.add(
@@ -205,8 +220,7 @@ module.exports = async function (deployer) {
       "",
       "https://github.com/relay3r-network/relay3r-jobs/blob/main/src/jobs/coreflasharbrelay3r.js"
     );
-  }
-  else if (DeployLiqMiner){
+  } else if (DeployLiqMiner) {
     //Init RLR Interface
     const RelayerTokenD = await Relay3rV2.at(Addrs.RLRToken[1]);
     //Deploy uniswap reward contract
@@ -216,29 +230,36 @@ module.exports = async function (deployer) {
     await deployer.deploy(RlrMooniRewards);
     const RlrMooniMine = await RlrMooniRewards.deployed();
 
-    if(testLiqMinerPhase) {
+    if (testLiqMinerPhase) {
       //deploy token to add liq to
-      await deployer.deploy(BurnableToken,"RelayerTestNew","RLRX");
+      await deployer.deploy(BurnableToken, "RelayerTestNew", "RLRX");
       const Token1 = await BurnableToken.deployed();
       //Set reward token
       await RlrUniMine.setRewardToken(Token1.address);
       //Mint 89k tokens to transfer to rewards
       await Token1.mint(Web3.utils.toWei("89000", "ether"));
       //Now transfer 89k tokens of token1 to reward pool
-      await Token1.transfer(RlrUniMine.address,Web3.utils.toWei("89000", "ether"));
+      await Token1.transfer(
+        RlrUniMine.address,
+        Web3.utils.toWei("89000", "ether")
+      );
       //Init slow rewardrate
       await RlrUniMine.initRewardSlow();
-    }
-    else {
+    } else {
       //Follow mainnet procedure
       //Send 4450 RLR to UniPool Rewards
-      RelayerTokenD.transfer(RlrUniMine.address,Web3.utils.toWei("4450", "ether"));
+      RelayerTokenD.transfer(
+        RlrUniMine.address,
+        Web3.utils.toWei("4450", "ether")
+      );
       //Send 4450 RLR to MooniPool Rewards
-      RelayerTokenD.transfer(RlrUniMine.RlrMooniMine,Web3.utils.toWei("4450", "ether"));
+      RelayerTokenD.transfer(
+        RlrUniMine.RlrMooniMine,
+        Web3.utils.toWei("4450", "ether")
+      );
       //Init slow rewards on both pools
       await RlrUniMine.initRewardSlow();
       await RlrMooniMine.initRewardSlow();
     }
   }
-
 };
