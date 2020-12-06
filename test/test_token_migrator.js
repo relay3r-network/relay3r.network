debc866c73b9c5442a3ab433b456bf6277051725
akshaynexus <akshaycm@hotmail.com>
2020-12-06 20:52:55 +0300

    Move Token migrator test from deploy to tests

---
+++ test/test_token_migrator.js
@@ -0,0 +1,46 @@
+var Web3 = require("web3");
+
+const TokenMigrator = artifacts.require("TokenMigratorCustomizable");
+//Mock testing token contract
+const BurnableToken = artifacts.require("BurnableToken");
+
+//Global vars
+//Tokens
+var Token1,Token2;
+//Migrator
+var TokenMigratorD;
+
+contract("TokenMigratorCustomizable", async function () {
+    it("Should Migrate tokens to V2", async () => {
+    //Deploy TokenMigrator
+    TokenMigratorD = await TokenMigrator.new();
+    assert(TokenMigratorD.address !== '');
+    ///Now deploy test token 1
+     Token1 = await BurnableToken.new("TestToken1", "TXS");
+     Token2 = await BurnableToken.new("TestToken2", "TXSS");
+     //Set swap and origin tokens
+     await TokenMigratorD.SetOriginToken(Token1.address);
+     await TokenMigratorD.SetSwapToken(Token2.address);
+
+    //Now transfer 1k tokens of token2 to migrator
+    await Token2.transfer(
+      TokenMigratorD.address,
+      Web3.utils.toWei("1000", "ether")
+    );
+    //Approve 1k tokens to migrator contract
+    await Token1.approve(
+      TokenMigratorD.address,
+      Web3.utils.toWei("1000", "ether")
+    );
+    //Swap 1k tokens to token2
+    await TokenMigratorD.swapTokens(Web3.utils.toWei("1000", "ether"));
+
+    const Token1SupplyAfterMigrate = await Token1.totalSupply();
+    const Token2SupplyAfterMigrate = await Token2.totalSupply();
+
+    //Check if migration was successful
+    assert(Token1SupplyAfterMigrate == 0); //Supply of token 1 to be 0 after migration
+    //supply of token 2 to be 1k after migration
+    assert(Token2SupplyAfterMigrate == Web3.utils.toWei("1000", "ether"));
+    });
+});
