e3bafbcc7711e1d0c523d1efc8c77f64956f3987
akshaynexus <akshaycm@hotmail.com>
2020-12-06 20:04:10 +0300

    Fix secrets.json not found error

---
+++ createmocksecrets.js
@@ -0,0 +1,6 @@
+const ethers = require("ethers");
+const fs = require("fs");
+
+const wallet = ethers.Wallet.createRandom();
+const data = {mnemonic:wallet.mnemonic.phrase,InfuraProjID:"1d1d1d"};
+fs.writeFileSync("secrets.json", JSON.stringify(data));
